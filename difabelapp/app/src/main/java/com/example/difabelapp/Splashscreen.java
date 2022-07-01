1b0e53d646c8398d17366689ec80ef1ad48b986f
hanifirvin <hanif.bro10@gmail.com>
2022-07-01 16:27:12 +0700

    splashscreen

---
+++ difabelapp/app/src/main/java/com/example/difabelapp/Splashscreen.java
@@ -0,0 +1,25 @@
+package com.example.difabelapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splashscreen.this,Login.class));
+                finish();
+            }
+        },1000);
+    }
+}
